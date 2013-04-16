b5fd3313e9c9b33fdeaa3bb79745805fbf8182c2
dexter <dexter@Kepler>
2013-04-16 17:11:53 +0200

    useless

--- flujos/src/ficheroAFecha/FicheroAFecha.java
+++ flujos/src/ficheroAFecha/FicheroAFecha.java
@@ -1,5 +1,11 @@
 package ficheroAFecha;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Date;
+
 /**
  * Clase FicheroAFecha
  * @author Rafael García Maliga
@@ -21,12 +27,40 @@ package ficheroAFecha;
  */
 public class FicheroAFecha {
 
-		
+	private static final File FILE = new File("fecha");
+	private static Date date = null;
+	
 	/**
 	 * @param args
+	 * @throws  
 	 */
 	public static void main(String[] args) {
-		
+		ObjectInputStream oi = null;
+		try {
+			oi = new ObjectInputStream(new FileInputStream(FILE));
+			date = (Date) oi.readObject();
+			System.out.format("Año: %tY %n" +
+					"Mes en letras: %<tB %n" +
+					"Mes en numero: %<tm %n" +
+					"Dia en letras: %<tA %n" +
+					"Dia en numero: %<td %n" +
+					"Hora: %<tH %n" +
+					"Minutos: %<tM %n" +
+					"Segundos: %<tS", date);
+		}catch(ClassNotFoundException e){	
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally{
+			if(oi != null){
+				try {
+					oi.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			date = null;
+		}
 	}
 
 }
